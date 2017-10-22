c05b63609e91f6883bb0b7810224ba4f9c2147ed
softberries <krzysztof.grajek@googlemail.com>
2017-10-22 11:34:22 +0200

    added scripts for data preparation

--- prepare-data.sh
+++ prepare-data.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
+S3_DESTINATION_FOLDER="grajo001log/users"
+SITES_FILE=sites.txt
 
+# https://archive.org/download/stackexchange
+
+IFS=$'\n'
+for site in `cat $SITES_FILE`
+do
+    echo "$site"
+    wget https://archive.org/download/stackexchange/$site.7z
+    7z x $site.7z -otemp
+    aws s3 cp temp/Users.xml s3://$S3_DESTINATION_FOLDER/$site.xml
+    rm $site.7z
+    rm -Rf temp
+done
+
+echo 'Finished!'
+exit 0
