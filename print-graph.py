852ba84b70126db08b92efdfa217b40fcb1a8cd3
softberries <krzysztof.grajek@googlemail.com>
2017-10-23 08:55:55 +0200

    added results of the scalding processing

--- print-graph.py
+++ print-graph.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.DataFrame.from_csv('part-00000', sep='\t', header=None)
-df.columns = ["Occurences"]
-print(df)
+##
+## Merge the output of multiple part-000* files into single file if necessary
+## with cat part-000* > scaldingdata.csv
+##
 
-ax = df.plot(kind='bar', title="Stackexchange Age Distribution", figsize=(15, 10), legend=True, fontsize=12)
+df = pd.DataFrame.from_csv('scaldingdata.csv', sep='\t', header=None, index_col=False)
+
+print(df.columns)
+
+df.columns = ['Age', 'Occurences']
+df = df.sort_values('Age')
+
+print(df)
+ax = df.set_index('Age').plot(kind='bar', title="Stackexchange Age Distribution", figsize=(15, 10), legend=True, fontsize=12)
 
 ax.set_xlabel("Age", fontsize=12)
 ax.set_ylabel("Occurences", fontsize=12)
-patches, labels = ax.get_legend_handles_labels()
-ax.legend(patches, labels, loc='best')
 
 plt.show()
